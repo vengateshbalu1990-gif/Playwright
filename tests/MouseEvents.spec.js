f51090207e5aa33c6a2ebfd56719cd6c5b3521e1
vengateshbalu1990-gif <vengatesh.balu1990@gmail.com>
2026-08-19 08:52:07 +0530

    Screenshot deleted

--- tests/MouseEvents.spec.js
+++ tests/MouseEvents.spec.js
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test'
-
-test('Drop Down',async({page})=>{
+// code for Mouse events
+test('Mouse events',async({page})=>{
 await page.goto('https://www.amazon.in/');
 await page.getByText('Account & Lists').hover();
 await page.locator('(//span[@class="nav-text"])[6]').click();
